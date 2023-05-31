659266f4fab7972a4f6079ca8fd44b86e01bcf12
hieu <labaohieu@gmail.com>
2023-05-31 14:36:49 +0700

    vehicle-drivers

--- cmd/noname-app/app/root.js
+++ cmd/noname-app/app/root.js
@@ -2,6 +2,7 @@ const Yargs = require("@server/lib/yargs");
 const Sequelize = require("@server/lib/sequelize");
 const { QueryTypes } = require("sequelize");
 const { v4: uuidv4 } = require("uuid");
+const DriverTemplate = require("./template");
 
 const Version = require("./version");
 Version.Use("1.0.0");
@@ -26,18 +27,50 @@ async function migrateHandler() {
   const tx = await vexDb.transaction();
 
   try {
-    if ([err1, err2].some(notNull)) throw Error("error from db");
+    if ([err1, err2].some(notNull)) throw Error("Error when connecting to db");
 
-    const vexTb = await vexDb.query("SELECT * FROM `asset_warehouses`", { type: QueryTypes.SELECT });
-    console.log("vexTb: ", vexTb[0]);
+    const queryDriver = `SELECT * FROM tbl_user join tbl_user_type on tbl_user.user_type = tbl_user_type.user_type_id where tbl_user_type.user_type_id in (12,13)`;
 
-    const query = `INSERT INTO asset_warehouses (id , name, description) VALUES ('${uuidv4()}', 'hieu', 'hieu')`;
-    // const vexIns = await vexDb.query(query, { type: QueryTypes.INSERT });
+    const maomaoTb = await maomaoDb.query(queryDriver, { type: QueryTypes.SELECT });
+    for (const [i, record] of maomaoTb.entries()) {
+      // if (i !== 0) break;
+      const full_name = record.full_name.split(" ").map(uppercaseFirstLetter);
+      const address = record.address?.split(" ").map(uppercaseFirstLetter).join(" ");
+      const driver = new DriverTemplate({
+        id: uuidv4(),
+        create_time: record.create_date,
+        update_time: record.update_date,
+        created_by: "",
+        last_updated_by: "",
+        deleted_by: "",
+        deleted_at: "",
+
+        first_name: full_name.slice(1).join(" "),
+        last_name: full_name[0],
+        date_of_birth: record.birthday,
+        national_id_card_no: record.identity_id || "chưa có" + i,
+        tax_id: null,
+        address: address,
+        phone: record.phone || "chưa có" + i,
+        email: record.email,
+        join_date: record.start_job,
+        exit_date: record.end_working_date,
+        advance_payment_limit: 0,
+        advance_payment_amount: 0,
+        deposit_amount: 0,
+        job_status: undefined,
+        driver_deposit_type_id: null,
+      });
+
+      const query = `INSERT INTO vehicle_drivers (${driver.getFields()}) VALUES (${driver.getValues()})`;
+      // console.log("query: ", query);
+      const _ = await vexDb.query(query, { transaction: tx, type: QueryTypes.INSERT });
+    }
 
     await tx.commit();
   } catch (error) {
     await tx.rollback();
-    console.log("error: ", error.message);
+    console.log("error: ", error);
   }
   var _ = await maomaoDb.close();
   var _ = await vexDb.close();
@@ -45,4 +78,8 @@ async function migrateHandler() {
 
 function notNull(e) {
   return e !== null;
+}
+
+function uppercaseFirstLetter(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
 }
