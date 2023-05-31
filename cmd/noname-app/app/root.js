9a6d21eb63d550bfabe25547ade6262ffdcaa8f5
hieu <labaohieu@gmail.com>
2023-05-31 14:58:06 +0700

    tyres

--- cmd/noname-app/app/root.js
+++ cmd/noname-app/app/root.js
@@ -3,6 +3,7 @@ const Sequelize = require("@server/lib/sequelize");
 const { QueryTypes } = require("sequelize");
 const { v4: uuidv4 } = require("uuid");
 const DriverTemplate = require("./template");
+const TyreTemplate = require("./tyreTemplate");
 
 const Version = require("./version");
 Version.Use("1.0.0");
@@ -29,15 +30,20 @@ async function migrateHandler() {
   try {
     if ([err1, err2].some(notNull)) throw Error("Error when connecting to db");
 
-    const queryDriver = `SELECT * FROM tbl_user join tbl_user_type on tbl_user.user_type = tbl_user_type.user_type_id where tbl_user_type.user_type_id in (12,13)`;
+    const queryTyre = `
+    SELECT tbl_tires.update_date, tbl_tire_name.tire_short_name, tbl_admin_tires_production.tire_production, tbl_admin_tires_size.tire_size_name, tbl_tires.tire_serial, tbl_tires.tire_distance_limit
+    FROM tbl_tires 
+    LEFT JOIN tbl_tire_name on tbl_tires.tire_name_id = tbl_tire_name.tire_name_id
+    LEFT JOIN tbl_admin_tires_production on tbl_tire_name.tire_name = tbl_admin_tires_production.tire_name_id
+    LEFT JOIN tbl_admin_tires_size on tbl_tire_name.tire_size_id = tbl_admin_tires_size.tire_size_id
+    `;
 
-    const maomaoTb = await maomaoDb.query(queryDriver, { type: QueryTypes.SELECT });
+    const maomaoTb = await maomaoDb.query(queryTyre, { type: QueryTypes.SELECT });
     for (const [i, record] of maomaoTb.entries()) {
-      // if (i !== 0) break;
-      const full_name = record.full_name.split(" ").map(uppercaseFirstLetter);
-      const address = record.address?.split(" ").map(uppercaseFirstLetter).join(" ");
-      const driver = new DriverTemplate({
-        id: uuidv4(),
+      if (i !== 1) continue;
+      const tyre_id = uuidv4();
+      const tyre = new TyreTemplate({
+        id: tyre_id,
         create_time: record.create_date,
         update_time: record.update_date,
         created_by: "",
@@ -45,26 +51,33 @@ async function migrateHandler() {
         deleted_by: "",
         deleted_at: "",
 
-        first_name: full_name.slice(1).join(" "),
-        last_name: full_name[0],
-        date_of_birth: record.birthday,
-        national_id_card_no: record.identity_id || "chưa có" + i,
-        tax_id: null,
-        address: address,
-        phone: record.phone || "chưa có" + i,
-        email: record.email,
-        join_date: record.start_job,
-        exit_date: record.end_working_date,
-        advance_payment_limit: 0,
-        advance_payment_amount: 0,
-        deposit_amount: 0,
-        job_status: undefined,
-        driver_deposit_type_id: null,
+        name: record.tire_production,
+        short_name: record.tire_short_name,
+        specification: record.tire_size_name,
+        serial_no: record.tire_serial,
+        operation_limit_km: record.tire_distance_limit,
+        replace_noti_max_threshold_km: 0,
+
+        used_km: "",
+        used_hours: "",
+        last_usage_caclulated_at: "",
       });
 
-      const query = `INSERT INTO vehicle_drivers (${driver.getFields()}) VALUES (${driver.getValues()})`;
-      // console.log("query: ", query);
-      const _ = await vexDb.query(query, { transaction: tx, type: QueryTypes.INSERT });
+      const warehouse = new WarehouseTemplate({
+        id: uuidv4(),
+        create_time: record.create_date,
+        update_time: record.update_date,
+
+        asset_warehouse_id: "",
+        tyre_id: tyre_id,
+      });
+
+      const tyreQuery = `INSERT INTO tyres (${tyre.getFields()}) VALUES (${tyre.getValues()})`;
+      const warehouseQuery = `INSERT INTO asset_warehouse_atyre_relations (${warehouse.getFields()}) VALUES (${warehouse.getValues()})`;
+      // console.log("tyreQuery: ", tyreQuery);
+      console.log("warehouseQuery: ", warehouseQuery);
+      // var _ = await vexDb.query(tyreQuery, { transaction: tx, type: QueryTypes.INSERT });
+      // var _ = await vexDb.query(warehouseQuery, { transaction: tx, type: QueryTypes.INSERT });
     }
 
     await tx.commit();
@@ -82,4 +95,26 @@ function notNull(e) {
 
 function uppercaseFirstLetter(word) {
   return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}
+
+class WarehouseTemplate {
+  constructor(data = {}) {
+    if (data.create_time) this.create_time = makeString(data.create_time);
+    if (data.update_time) this.update_time = makeString(data.update_time);
+
+    if (data.id) this.id = makeString(data.id);
+    if (data.asset_warehouse_id) this.asset_warehouse_id = makeString(data.asset_warehouse_id);
+    if (data.tyre_id) this.tyre_id = makeString(data.tyre_id);
+  }
+
+  getFields() {
+    return Object.keys(this).join(", ");
+  }
+  getValues() {
+    return Object.values(this).join(", ");
+  }
+}
+
+function makeString(value) {
+  return `'${String(value.trim() || "")}'`;
 }
