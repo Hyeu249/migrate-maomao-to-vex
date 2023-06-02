1e7394dbce377da999168f016c0c3d1cc21a49e0
hieu <labaohieu@gmail.com>
2023-06-02 08:41:45 +0700

    vehicle-types

--- cmd/noname-app/app/root.js
+++ cmd/noname-app/app/root.js
@@ -2,8 +2,7 @@ const Yargs = require("@server/lib/yargs");
 const Sequelize = require("@server/lib/sequelize");
 const { QueryTypes } = require("sequelize");
 const { v4: uuidv4 } = require("uuid");
-const DriverTemplate = require("./template");
-const TyreTemplate = require("./tyreTemplate");
+const VehicleTypeTemplate = require("./vehicleTypeTemplate");
 
 const Version = require("./version");
 Version.Use("1.0.0");
@@ -30,19 +29,16 @@ async function migrateHandler() {
   try {
     if ([err1, err2].some(notNull)) throw Error("Error when connecting to db");
 
-    const queryTyre = `
-    SELECT tbl_tires.update_date, tbl_tire_name.tire_short_name, tbl_admin_tires_production.tire_production, tbl_admin_tires_size.tire_size_name, tbl_tires.tire_serial, tbl_tires.tire_distance_limit
-    FROM tbl_tires 
-    LEFT JOIN tbl_tire_name on tbl_tires.tire_name_id = tbl_tire_name.tire_name_id
-    LEFT JOIN tbl_admin_tires_production on tbl_tire_name.tire_name = tbl_admin_tires_production.tire_name_id
-    LEFT JOIN tbl_admin_tires_size on tbl_tire_name.tire_size_id = tbl_admin_tires_size.tire_size_id
+    const query = `
+      SELECT * FROM tbl_car_type 
+      WHERE work_status = 1
     `;
 
-    const maomaoTb = await maomaoDb.query(queryTyre, { type: QueryTypes.SELECT });
+    const maomaoTb = await maomaoDb.query(query, { type: QueryTypes.SELECT });
     for (const [i, record] of maomaoTb.entries()) {
-      if (i !== 1) continue;
+      // if (i !== 1) continue;
       const tyre_id = uuidv4();
-      const tyre = new TyreTemplate({
+      const tyre = new VehicleTypeTemplate({
         id: tyre_id,
         create_time: record.create_date,
         update_time: record.update_date,
@@ -51,33 +47,13 @@ async function migrateHandler() {
         deleted_by: "",
         deleted_at: "",
 
-        name: record.tire_production,
-        short_name: record.tire_short_name,
-        specification: record.tire_size_name,
-        serial_no: record.tire_serial,
-        operation_limit_km: record.tire_distance_limit,
-        replace_noti_max_threshold_km: 0,
-
-        used_km: "",
-        used_hours: "",
-        last_usage_caclulated_at: "",
+        name: record.name,
+        description: "",
       });
 
-      const warehouse = new WarehouseTemplate({
-        id: uuidv4(),
-        create_time: record.create_date,
-        update_time: record.update_date,
-
-        asset_warehouse_id: "",
-        tyre_id: tyre_id,
-      });
-
-      const tyreQuery = `INSERT INTO tyres (${tyre.getFields()}) VALUES (${tyre.getValues()})`;
-      const warehouseQuery = `INSERT INTO asset_warehouse_atyre_relations (${warehouse.getFields()}) VALUES (${warehouse.getValues()})`;
-      // console.log("tyreQuery: ", tyreQuery);
-      console.log("warehouseQuery: ", warehouseQuery);
-      // var _ = await vexDb.query(tyreQuery, { transaction: tx, type: QueryTypes.INSERT });
-      // var _ = await vexDb.query(warehouseQuery, { transaction: tx, type: QueryTypes.INSERT });
+      const vehicleTypeQuery = `INSERT INTO vehicle_types (${tyre.getFields()}) VALUES (${tyre.getValues()})`;
+      // console.log("vehicleTypeQuery: ", vehicleTypeQuery);
+      var _ = await vexDb.query(vehicleTypeQuery, { transaction: tx, type: QueryTypes.INSERT });
     }
 
     await tx.commit();
